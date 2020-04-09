b97c7306d2e8501e3060e5c35381f8fa8c37470a
chippison <chip@seekom.com>
2020-04-09 15:39:34 +1200

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Users from '../components/Users';
 
 Vue.use(VueRouter);
 
@@ -18,7 +19,13 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  },
+  {
+    path: "/users",
+    name: "Users",
+    component: Users
+  },
+
 ];
 
 const router = new VueRouter({
