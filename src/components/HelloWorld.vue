b623c4236dc8d2000fe23bde3f0565d034560c02
chippison <chip@seekom.com>
2020-04-17 14:42:14 +1200

    my coomit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
+      Change 1 recipes on how to configure / customize this project,<br />
       check out thesdasd
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
